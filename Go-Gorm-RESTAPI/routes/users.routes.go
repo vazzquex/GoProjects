ebf574f6f8c5fa4c940877253aa8673e7cf8958f
Santiago <vazquezgrillo.santiago@gmail.com>
2023-06-20 17:20:37 -0300

    PostUsersHandler

--- Go-Gorm-RESTAPI/routes/users.routes.go
+++ Go-Gorm-RESTAPI/routes/users.routes.go
@@ -1,6 +1,11 @@
 package routes
 
-import "net/http"
+import (
+	"encoding/json"
+	"net/http"
+
+	"github.com/vazzquex/go-gorm-restapi/models"
+)
 
 func GetUsersHandler(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("get users"))
@@ -11,6 +16,11 @@ func GetUserHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func PostUsersHandler(w http.ResponseWriter, r *http.Request) {
+
+	var user = models.User
+
+	json.NewDecoder(r.Body).Decode(&user)
+
 	w.Write([]byte("Post"))
 }
 
