fef9809ca3e28f8327d6766e971b0f79979935a0
Santiago <vazquezgrillo.santiago@gmail.com>
2023-06-20 01:16:18 -0300

    create routes and models

--- Go-Gorm-RESTAPI/main.go
+++ Go-Gorm-RESTAPI/main.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/gorilla/mux"
 	"github.com/vazzquex/go-gorm-restapi/db"
+	"github.com/vazzquex/go-gorm-restapi/models"
 	"github.com/vazzquex/go-gorm-restapi/routes"
 )
 
@@ -12,9 +13,17 @@ func main() {
 
 	db.DBConnection()
 
+	db.DB.AutoMigrate(models.Task{})
+	db.DB.AutoMigrate(models.User{})
+
 	r := mux.NewRouter()
 
 	r.HandleFunc("/", routes.HomeHandler)
+
+	r.HandleFunc("/users", routes.GetUsersHandler).Methods("GET")
+	r.HandleFunc("/users/{id}", routes.GetUserHandler).Methods("GET")
+	r.HandleFunc("/users", routes.PostUsersHandler).Methods("POST")
+	r.HandleFunc("/users", routes.DeleteUserHandler).Methods("DELETE")
 
 	http.ListenAndServe(":3000", r)
 
--- Go-Gorm-RESTAPI/routes/users.routes.go
+++ Go-Gorm-RESTAPI/routes/users.routes.go
@@ -1,1 +1,19 @@
 package routes
+
+import "net/http"
+
+func GetUsersHandler(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("get users"))
+}
+
+func GetUserHandler(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("get user"))
+}
+
+func PostUsersHandler(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Post"))
+}
+
+func DeleteUserHandler(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Delete"))
+}
